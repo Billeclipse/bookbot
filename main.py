99c916a2e1d418d3024a406a44b8a468def22f04
vasiliosgio <vasiliosgio@hotmail.com>
2026-03-25 20:27:45 +0200

    made the latest changes to the bookbot project

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import get_num_words
+import sys
+from stats import get_num_words, get_char_count, sort_characters
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,9 +8,28 @@ def get_book_text(filepath):
 
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+    text = get_book_text(filepath)
+
     num_words = get_num_words(text)
+    char_counts = get_char_count(text)
+    sorted_chars = sort_characters(char_counts)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for item in sorted_chars:
+        if item["char"].isalpha():
+            print(f'{item["char"]}: {item["num"]}')
+
+    print("============= END ===============")
 
 
 main()
